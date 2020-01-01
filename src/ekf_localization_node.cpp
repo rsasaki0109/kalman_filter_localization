ec07bca37b993ae3b6f98eeaddc1215a0335bca9
Ryohei Sasaki <rsasaki0109@gmail.com>
2020-01-02 00:00:00 +0900

    Initial commit

---
+++ src/ekf_localization_node.cpp
@@ -0,0 +1,12 @@
+#include <kalman_filter_localization/ekf_localization_component.h>
+//#include <ekf_localization_component.h>
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<kalman_filter_localization::EkfLocalizationComponent>(options);
+    rclcpp::spin(component);
+    rclcpp::shutdown();
+    return 0;
+}
