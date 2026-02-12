e2ba42a5be2cb0ce9fab070e7a64b5e61e7f1aac
Ryohei Sasaki <rsasaki0109@gmail.com>
2026-02-13 00:00:00 +0900

    refactor: extract visibility control header

--- include/kalman_filter_localization/visibility_control.hpp
+++ include/kalman_filter_localization/visibility_control.hpp
@@ -29,27 +29,37 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#ifndef KALMAN_FILTER_LOCALIZATION__EKF_LOCALIZATION_COMPONENT_HPP_
-#define KALMAN_FILTER_LOCALIZATION__EKF_LOCALIZATION_COMPONENT_HPP_
+#ifndef KALMAN_FILTER_LOCALIZATION__VISIBILITY_CONTROL_HPP_
+#define KALMAN_FILTER_LOCALIZATION__VISIBILITY_CONTROL_HPP_
 
-#include <memory>
+// The below macros are taken from https://gcc.gnu.org/wiki/Visibility and from
+// demos/composition/include/composition/visibility_control.h at https://github.com/ros2/demos
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define KFL_EKFL_EXPORT __attribute__ ((dllexport))
+    #define KFL_EKFL_IMPORT __attribute__ ((dllimport))
+  #else
+    #define KFL_EKFL_EXPORT __declspec(dllexport)
+    #define KFL_EKFL_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef KFL_EKFL_BUILDING_DLL
+    #define KFL_EKFL_PUBLIC KFL_EKFL_EXPORT
+  #else
+    #define KFL_EKFL_PUBLIC KFL_EKFL_IMPORT
+  #endif
+  #define KFL_EKFL_PUBLIC_TYPE KFL_EKFL_PUBLIC
+  #define KFL_EKFL_LOCAL
+#else
+  #define KFL_EKFL_EXPORT __attribute__ ((visibility("default")))
+  #define KFL_EKFL_IMPORT
+  #if __GNUC__ >= 4
+    #define KFL_EKFL_PUBLIC __attribute__ ((visibility("default")))
+    #define KFL_EKFL_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define KFL_EKFL_PUBLIC
+    #define KFL_EKFL_LOCAL
+  #endif
+  #define KFL_EKFL_PUBLIC_TYPE
+#endif
 
-#include <kalman_filter_localization/visibility_control.hpp>
-
-#include <rclcpp/rclcpp.hpp>
-
-namespace kalman_filter_localization
-{
-class KFL_EKFL_PUBLIC EkfLocalizationComponent : public rclcpp::Node
-{
-public:
-  explicit EkfLocalizationComponent(const rclcpp::NodeOptions & options);
-  ~EkfLocalizationComponent() override;
-
-private:
-  struct Impl;
-  std::unique_ptr<Impl> impl_;
-};
-}  // namespace kalman_filter_localization
-
-#endif  // KALMAN_FILTER_LOCALIZATION__EKF_LOCALIZATION_COMPONENT_HPP_
+#endif  // KALMAN_FILTER_LOCALIZATION__VISIBILITY_CONTROL_HPP_
